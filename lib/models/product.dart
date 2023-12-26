7bcf5a3d44d7119fda5ff9860298e72eaf69312b
fikireab-m <fikireab.meku@gmail.com>
2023-12-26 20:36:57 +0300

    data models

---
+++ lib/models/product.dart
@@ -0,0 +1,50 @@
+class Product {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  List<String>? images;
+  String? creationAt;
+  String? updatedAt;
+  Category? category;
+
+  Product(
+      {this.id,
+      this.title,
+      this.price,
+      this.description,
+      this.images,
+      this.creationAt,
+      this.updatedAt,
+      this.category});
+
+  Product.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    description = json['description'];
+    images = json['images'].cast<String>();
+    creationAt = json['creationAt'];
+    updatedAt = json['updatedAt'];
+    category =
+        json['category'] != null ? Category.fromJson(json['category']) : null;
+  }
+}
+
+class Category {
+  int? id;
+  String? name;
+  String? image;
+  String? creationAt;
+  String? updatedAt;
+
+  Category({this.id, this.name, this.image, this.creationAt, this.updatedAt});
+
+  Category.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    image = json['image'];
+    creationAt = json['creationAt'];
+    updatedAt = json['updatedAt'];
+  }
+}
