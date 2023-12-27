de0e0e390dcee457c688d9f7a083c2a6a699d477
fikireab-m <fikireab.meku@gmail.com>
2023-12-27 15:24:44 +0300

    get categories upon request

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -2,62 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
 import 'package:flutter_shop/constants/colors.dart';
+import 'package:flutter_shop/data/api_call.dart';
 import 'package:flutter_shop/models/category.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  final List<Category> categories;
-  const CategoriesScreen({Key? key, required this.categories})
-      : super(key: key);
+class CategoriesScreen extends StatefulWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  Future<List<Category>> getCategories() async {
+    final categories = await APIHandler.getCategories();
+    return categories;
+  }
+
+  @override
+  void initState() {
+    getCategories();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: const Text("Categories")),
-      body: GridView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: categories.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 0.0,
-            mainAxisSpacing: 0.0,
-            childAspectRatio: 1.2,
-          ),
-          itemBuilder: (ctx, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: FancyShimmerImage(
-                      height: size.width * 0.45,
-                      width: size.width * 0.45,
-                      errorWidget: const Icon(
-                        IconlyBold.danger,
-                        color: Colors.red,
-                        size: 28,
-                      ),
-                      imageUrl: categories[index].image ?? "",
-                      boxFit: BoxFit.fill,
+      body: FutureBuilder<List<Category>>(
+          future: getCategories(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final categories = snapshot.data!;
+            return GridView.builder(
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(),
+                itemCount: categories.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 0.0,
+                  mainAxisSpacing: 0.0,
+                  childAspectRatio: 1.2,
+                ),
+                itemBuilder: (ctx, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: FancyShimmerImage(
+                            height: size.width * 0.45,
+                            width: size.width * 0.45,
+                            errorWidget: const Icon(
+                              IconlyBold.danger,
+                              color: Colors.red,
+                              size: 28,
+                            ),
+                            imageUrl: categories[index].image ?? "",
+                            boxFit: BoxFit.fill,
+                          ),
+                        ),
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            categories[index].name ?? "",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              backgroundColor: lightCardColor.withOpacity(0.5),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      categories[index].name ?? "",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        backgroundColor: lightCardColor.withOpacity(0.5),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
+                  );
+                });
           }),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_shop/constants/colors.dart';
 import 'package:flutter_shop/data/api_call.dart';
-import 'package:flutter_shop/models/category.dart';
 import 'package:flutter_shop/models/product.dart';
 import 'package:flutter_shop/screens/categories_screen.dart';
 import 'package:flutter_shop/screens/feeds_screen.dart';
@@ -28,17 +27,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return products;
   }
 
-  List<Category> categories = [];
-  Future<void> getCategories() async {
-    categories = await APIHandler.getCategories();
-    setState(() {});
-  }
-
   @override
   void initState() {
     _textEditingController = TextEditingController();
     getProducts();
-    getCategories();
     setState(() {});
     super.initState();
   }
@@ -66,7 +58,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 context,
                 PageTransition(
                   type: PageTransitionType.fade,
-                  child: CategoriesScreen(categories: categories),
+                  child: const CategoriesScreen(),
                 ),
               );
             },
