49331563474659e01bc656cdd34d555df5972515
fikireab-m <fikireab.meku@gmail.com>
2023-12-27 16:04:36 +0300

    user model

---
+++ lib/models/user.dart
@@ -0,0 +1,31 @@
+class User {
+  int? id;
+  String? email;
+  String? password;
+  String? name;
+  String? role;
+  String? avatar;
+  String? creationAt;
+  String? updatedAt;
+
+  User(
+      {this.id,
+      this.email,
+      this.password,
+      this.name,
+      this.role,
+      this.avatar,
+      this.creationAt,
+      this.updatedAt});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    email = json['email'];
+    password = json['password'];
+    name = json['name'];
+    role = json['role'];
+    avatar = json['avatar'];
+    creationAt = json['creationAt'];
+    updatedAt = json['updatedAt'];
+  }
+}
